6ec36f9ff67c510eaf293b49436f7937b9d8c4dc
laylindo <layla-sep@hotmai.com>
2020-10-01 13:06:48 -0300

    Add answers to problems 1000 and 10001

---
+++ src/OnlineJudge/Problem1001.java
@@ -0,0 +1,16 @@
+package OnlineJudge;
+
+import java.util.Scanner;
+
+public class Problem1001 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        int X = A+B;
+        System.out.println("X = " + X);
+    }
+}
